8f28bf67bad5d78e34dff3563958a450f381f16c
MysteryPancake <Mysterypancake1@users.noreply.github.com>
2017-05-14 20:53:19 +1000

    Update and rename fundeath.lua to funkill.lua

--- fun/funkill.lua
+++ fun/funkill.lua
@@ -1,4 +1,4 @@
-hook.Add( "EntityTakeDamage", "FunDeath", function( target, dmg )
+hook.Add( "EntityTakeDamage", "FunKill", function( target, dmg )
 	dmg:AddDamage( target:Health() )
 	dmg:SetDamageForce( dmg:GetAttacker():GetForward() * 9999 + Vector( 0, 0, 9999 ) )
 end )
