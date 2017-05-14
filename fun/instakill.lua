ea53f55e6ce8d036adadb3274517b9692f106f82
MysteryPancake <Mysterypancake1@users.noreply.github.com>
2017-05-14 21:13:14 +1000

    Update and rename funkill.lua to instakill.lua

--- fun/instakill.lua
+++ fun/instakill.lua
@@ -1,4 +1,4 @@
-hook.Add( "EntityTakeDamage", "FunKill", function( target, dmg )
+hook.Add( "EntityTakeDamage", "Instakill", function( target, dmg )
 	dmg:AddDamage( target:Health() )
 	dmg:SetDamageForce( dmg:GetDamageForce() + dmg:GetAttacker():GetForward() * 9999 + Vector( 0, 0, 9999 ) )
 end )
