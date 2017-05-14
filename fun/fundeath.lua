e23d70c75546304b9f09a6451ffdf8426c561254
MysteryPancake <Mysterypancake1@users.noreply.github.com>
2017-05-14 20:52:28 +1000

    Update fundeath.lua

--- fun/fundeath.lua
+++ fun/fundeath.lua
@@ -1,4 +1,4 @@
 hook.Add( "EntityTakeDamage", "FunDeath", function( target, dmg )
-	dmg:ScaleDamage( 99999999999999999999999999999999999999999999999 )
+	dmg:AddDamage( target:Health() )
 	dmg:SetDamageForce( dmg:GetAttacker():GetForward() * 9999 + Vector( 0, 0, 9999 ) )
 end )
