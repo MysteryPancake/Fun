d8652311c7c6d0091080f2f162a3f6003aa13c6e
MysteryPancake <Mysterypancake1@users.noreply.github.com>
2017-05-14 20:12:43 +1000

    Delete fundeath.lua

--- lua/autorun/server/fundeath.lua
+++
@@ -1,4 +0,0 @@
-hook.Add( "EntityTakeDamage", "FunDeath", function( target, dmg )
-	dmg:ScaleDamage( 99999999999999999999999999999999999999999999999 )
-	dmg:SetDamageForce( dmg:GetAttacker():GetForward() * 9999 + Vector( 0, 0, 9999 ) )
-end )
