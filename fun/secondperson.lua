7a9d46f80e4894f1dfaeee3f022d1ad643c4faec
MysteryPancake <Mysterypancake1@users.noreply.github.com>
2017-05-14 21:13:58 +1000

    Update secondperson.lua

--- fun/secondperson.lua
+++ fun/secondperson.lua
@@ -2,7 +2,7 @@ CreateClientConVar( "secondperson", "0", true, false )
 local enabled = GetConVar( "secondperson" )
 
 hook.Add( "CalcView", "SecondPersonView", function( ply, pos, ang, fov )
-  if ply:GetViewEntity() ~= ply or !enabled:GetBool() then return end
-  local eyes = ply:GetAttachment( ply:LookupAttachment( "eyes" ) )
-  return { origin = eyes.Pos, angles = ang, fov = fov, drawviewer = true }
+    if ply:GetViewEntity() ~= ply or !enabled:GetBool() then return end
+    local eyes = ply:GetAttachment( ply:LookupAttachment( "eyes" ) )
+    return { origin = eyes.Pos, angles = ang, fov = fov, drawviewer = true }
 end )
