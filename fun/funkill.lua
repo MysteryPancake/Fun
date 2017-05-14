c4325bfd54c4453d21dea615f0c83bee88927d3e
MysteryPancake <Mysterypancake1@users.noreply.github.com>
2017-05-14 20:57:41 +1000

    Update funkill.lua

--- fun/funkill.lua
+++ fun/funkill.lua
@@ -1,4 +1,4 @@
 hook.Add( "EntityTakeDamage", "FunKill", function( target, dmg )
 	dmg:AddDamage( target:Health() )
-	dmg:SetDamageForce( dmg:GetAttacker():GetForward() * 9999 + Vector( 0, 0, 9999 ) )
+	dmg:SetDamageForce( dmg:GetDamageForce() + dmg:GetAttacker():GetForward() * 9999 + Vector( 0, 0, 9999 ) )
 end )
