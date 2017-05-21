3095569121b8c5a0d0f3f14cfdef28cfdb5ec530
MysteryPancake <Mysterypancake1@users.noreply.github.com>
2017-05-21 19:38:48 +1000

    tabs

--- fun/secondperson.lua
+++ fun/secondperson.lua
@@ -6,16 +6,16 @@ local usepos = GetConVar( "secondperson_pos" )
 local useang = GetConVar( "secondperson_ang" )
 
 hook.Add( "CalcView", "SecondPersonView", function( ply, pos, ang, fov )
-  if ply:GetViewEntity() ~= ply or !enabled:GetBool() then return end
-  local eyes = ply:GetAttachment( ply:LookupAttachment( "eyes" ) )
-  return { origin = usepos:GetBool() and eyes.Pos or pos, angles = useang:GetBool() and eyes.Ang or ang, fov = fov, drawviewer = true }
+	if ply:GetViewEntity() ~= ply or !enabled:GetBool() then return end
+	local eyes = ply:GetAttachment( ply:LookupAttachment( "eyes" ) )
+	return { origin = usepos:GetBool() and eyes.Pos or pos, angles = useang:GetBool() and eyes.Ang or ang, fov = fov, drawviewer = true }
 end )
 
 hook.Add( "PopulateToolMenu", "SecondPersonSettings", function()
-  spawnmenu.AddToolMenuOption( "Options", "View", "Second_Person_Options", "Second Person", "", "", function( panel )
-    panel:ClearControls()
-    panel:CheckBox( "Enabled", "secondperson" )
-    panel:CheckBox( "Use eye position", "secondperson_pos" )
-    panel:CheckBox( "Use eye angles", "secondperson_ang" )
-  end )
+	spawnmenu.AddToolMenuOption( "Options", "View", "Second_Person_Options", "Second Person", "", "", function( panel )
+		panel:ClearControls()
+		panel:CheckBox( "Enabled", "secondperson" )
+		panel:CheckBox( "Use eye position", "secondperson_pos" )
+		panel:CheckBox( "Use eye angles", "secondperson_ang" )
+	end )
 end )
