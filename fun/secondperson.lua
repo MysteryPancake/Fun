44544d00bafdc4308bb0cdec7b57f4cd8cc36e70
MysteryPancake <Mysterypancake1@users.noreply.github.com>
2017-06-17 19:31:57 +1000

    camelCase

--- fun/secondperson.lua
+++ fun/secondperson.lua
@@ -2,13 +2,13 @@ CreateClientConVar( "secondperson", "1", true, false )
 CreateClientConVar( "secondperson_pos", "1", true, false )
 CreateClientConVar( "secondperson_ang", "0", true, false )
 local enabled = GetConVar( "secondperson" )
-local usepos = GetConVar( "secondperson_pos" )
-local useang = GetConVar( "secondperson_ang" )
+local usePos = GetConVar( "secondperson_pos" )
+local useAng = GetConVar( "secondperson_ang" )
 
 hook.Add( "CalcView", "SecondPersonView", function( ply, pos, ang, fov )
 	if ply:GetViewEntity() ~= ply or !enabled:GetBool() then return end
 	local eyes = ply:GetAttachment( ply:LookupAttachment( "eyes" ) )
-	return { origin = usepos:GetBool() and eyes.Pos or pos, angles = useang:GetBool() and eyes.Ang or ang, fov = fov, drawviewer = true }
+	return { origin = usePos:GetBool() and eyes.Pos or pos, angles = useAng:GetBool() and eyes.Ang or ang, fov = fov, drawviewer = true }
 end )
 
 hook.Add( "PopulateToolMenu", "SecondPersonSettings", function()
