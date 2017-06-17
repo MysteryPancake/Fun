26d6879942730bab649675b511552a18900f2dd9
MysteryPancake <Mysterypancake1@users.noreply.github.com>
2017-06-17 17:22:25 +1000

    Update past.lua

--- fun/past.lua
+++ fun/past.lua
@@ -1,8 +1,8 @@
 local past = {}
 
-hook.Add( "CalcView", "PastView", function( ply, pos, ang )
+hook.Add( "CalcView", "PastView", function( ply, pos, ang, fov )
 	while #past < 100 do
-		table.insert( past, { origin = pos, angles = ang, drawviewer = true } )
+		table.insert( past, { origin = pos, angles = ang, fov = fov, drawviewer = true } )
 	end
 	table.remove( past, 1 )
 	return past[ 1 ]
