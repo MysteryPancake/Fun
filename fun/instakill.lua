811b7ea2fb48e0d8cffa3ffc2a564c1798df129e
MysteryPancake <Mysterypancake1@users.noreply.github.com>
2017-06-18 18:32:13 +1000

    Update instakill.lua

--- fun/instakill.lua
+++ fun/instakill.lua
@@ -1,4 +1,4 @@
-hook.Add( "EntityTakeDamage", "Instakill", function( target, dmg )
+hook.Add( "EntityTakeDamage", "InstaKill", function( target, dmg )
 	dmg:AddDamage( target:Health() )
 	dmg:SetDamageForce( dmg:GetDamageForce() + dmg:GetAttacker():GetForward() * 9999 + Vector( 0, 0, 9999 ) )
 end )
