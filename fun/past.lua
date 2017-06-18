2b139a46ac0352ecc5fec145c8187abc0431b3bc
MysteryPancake <Mysterypancake1@users.noreply.github.com>
2017-06-18 10:49:02 +1000

    Maybe faster

--- fun/past.lua
+++ fun/past.lua
@@ -1,7 +1,7 @@
 local past = {}
 
 hook.Add( "CalcView", "PastView", function( ply, pos, ang, fov )
-	while #past < 100 do
+	for _ = 1, next( past ) == nil and 100 or 1 do
 		table.insert( past, { origin = pos, angles = ang, fov = fov, drawviewer = true } )
 	end
 	table.remove( past, 1 )
