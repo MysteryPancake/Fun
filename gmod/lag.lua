cb2c508f98d915a038264f9b3e5103abb106d893
Mystery Pancake <MysteryPancake@users.noreply.github.com>
2017-06-27 20:49:34 +1000

    Create lag.lua

---
+++ gmod/lag.lua
@@ -0,0 +1,10 @@
+local lag = 2
+local lagPos = Angle( 0, 0, 0 )
+local lagAng = Angle( 0, 0, 0 )
+hook.Add( 'CalcView', 'Lag', function( ply, pos, ang )
+	if math.random( lag ) == 1 then
+		lagPos = origin
+		lagAng = angles
+	end
+	return { origin = lagPos, angles = lagAng }
+end )
