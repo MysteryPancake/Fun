d818a8066a6772d594c823a65b2f64f3aa724220
Mystery Pancake <MysteryPancake@users.noreply.github.com>
2017-07-30 13:47:45 +1000

    Update discord.lua

--- lua/discord.lua
+++ lua/discord.lua
@@ -1,10 +1,6 @@
 local str = "My name is Jeff"
 
 for i = 1, #str do
-    local c = string.lower( string.sub( str, i, i ) )
-	if c == " " then
-		io.write( " " )
-	else
-   		io.write( ":regional_indicator_" .. c .. ": ")
-	end
+	local c = string.lower( string.sub( str, i, i ) )
+	io.write( c == " " and " " or ":regional_indicator_" .. c .. ": " )
 end
