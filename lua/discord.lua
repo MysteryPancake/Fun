58b023063274dbc5b58a19d2a597210bc01fbbf8
Mystery Pancake <MysteryPancake@users.noreply.github.com>
2017-07-30 14:02:40 +1000

    Update discord.lua

--- lua/discord.lua
+++ lua/discord.lua
@@ -1,6 +1,26 @@
-local str = "My name is Jeff"
+local str = "22 Jump Street is the best movie ever made"
+
+local numbers = {
+	[ 0 ] = ":zero: ",
+	[ 1 ] = ":one: ",
+	[ 2 ] = ":two: ",
+	[ 3 ] = ":three: ",
+	[ 4 ] = ":four: ",
+	[ 5 ] = ":five: ",
+	[ 6 ] = ":six: ",
+	[ 7 ] = ":seven: ",
+	[ 8 ] = ":eight: ",
+	[ 9 ] = ":nine: "
+}
 
 for i = 1, #str do
 	local c = string.lower( string.sub( str, i, i ) )
-	io.write( c == " " and " " or ":regional_indicator_" .. c .. ": " )
+	local n = tonumber( c )
+	if n then
+		io.write( numbers[ n ] )
+	elseif c == " " then
+		io.write( " " )
+	else
+		io.write( ":regional_indicator_" .. c .. ": " )
+	end
 end
