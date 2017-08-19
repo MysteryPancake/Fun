5866899f1bd2846e4f133db7f6727664c9873f07
Mystery Pancake <MysteryPancake@users.noreply.github.com>
2017-08-19 16:52:57 +1000

    Update webhook.lua

--- gmod/webhook.lua
+++ gmod/webhook.lua
@@ -1,3 +1,3 @@
 hook.Add( "PlayerSay", "Discord", function( ply, text )
-	http.Post( "https://discordapp.com/api/webhooks/WEBHOOK_ID/WEBHOOK_TOKEN", { content = text, username = ply:Nick() } )
+	http.Post( "https://discordapp.com/api/webhooks/WEBHOOK_ID_HERE/WEBHOOK_TOKEN_HERE", { content = text, username = ply:Nick() } )
 end )
