195d6418a6f137aaa7933d9c97e078d3664940cd
Mystery Pancake <MysteryPancake@users.noreply.github.com>
2017-09-09 15:46:51 +1000

    Create photorealer.lua

---
+++ gmod/photorealer.lua
@@ -0,0 +1,47 @@
+local pos, ang
+local rendering = false
+local rt = GetRenderTarget( "PhotoReal", ScrW(), ScrH(), true )
+local mat = CreateMaterial( "UnlitGeneric", "GMODScreenspace", {
+	[ "$basetexturetransform" ] = "center .5 .5 scale -1 -1 rotate 0 translate 0 0",
+	[ "$texturealpha" ] = "0",
+	[ "$vertexalpha" ] = "1"
+} )
+
+hook.Add( "PostDrawOpaqueRenderables", "PhotoReal", function()
+
+	if rendering then return end
+
+	if pos and ang then
+
+		local oldRT = render.GetRenderTarget()
+
+		render.SetRenderTarget( rt )
+
+			render.Clear( 0, 0, 0, 255 )
+			render.ClearDepth()
+			render.ClearStencil()
+
+			rendering = true
+			render.RenderView( {
+				x = 0, y = 0, w = ScrW(), h = ScrH(),
+				origin = pos, angles = ang
+			} )
+			rendering = false
+
+		render.SetRenderTarget( oldRT )
+
+		cam.Start3D2D( pos + ang:Forward() * ( math.sqrt( ScrW() * ScrH() ) / 2 ) + ang:Right() * -( ScrW() / 2 ) + ang:Up() * ( ScrH() / 2 ), Angle( 0, ang.y - 90, -ang.p + 90 ), 1 )
+			mat:SetTexture( "$basetexture", rt )
+			surface.SetDrawColor( 255, 255, 255 )
+			surface.SetMaterial( mat )
+			surface.DrawTexturedRect( 0, 0, ScrW(), ScrH() )
+		cam.End3D2D()
+
+	end
+
+end )
+
+concommand.Add( "PhotoReal", function()
+	pos = EyePos()
+	ang = EyeAngles()
+end )
