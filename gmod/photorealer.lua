0911d1020a4f75fcc75119c74a8f84a9a6e8806f
Mystery Pancake <MysteryPancake@users.noreply.github.com>
2017-09-09 23:08:41 +1000

    Push and pop make things simpler

--- gmod/photorealer.lua
+++ gmod/photorealer.lua
@@ -13,19 +13,15 @@ hook.Add( "PostDrawOpaqueRenderables", "PhotoReal", function()
 
 	if pos and ang then
 
-		local oldRT = render.GetRenderTarget()
+		render.PushRenderTarget( rt )
 
-		render.SetRenderTarget( rt )
-
-			render.Clear( 0, 0, 0, 255 )
-			render.ClearDepth()
-			render.ClearStencil()
+			render.Clear( 0, 0, 0, 255, true, true )
 
 			rendering = true
 			render.RenderView( { x = 0, y = 0, w = ScrW(), h = ScrH(), origin = pos, angles = ang } )
 			rendering = false
 
-		render.SetRenderTarget( oldRT )
+		render.PopRenderTarget()
 
 		cam.Start3D2D( pos + ang:Forward() * ( math.sqrt( ScrW() * ScrH() ) / 2 ) + ang:Right() * -( ScrW() / 2 ) + ang:Up() * ( ScrH() / 2 ), Angle( 0, ang.y - 90, -ang.p + 90 ), 1 )
 			mat:SetTexture( "$basetexture", rt )
