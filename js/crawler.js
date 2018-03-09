6642dc4e6d5364068399700d437eb94a6a2e0d6b
MysteryPancake <MysteryPancake@users.noreply.github.com>
2018-03-09 23:33:04 +1100

    this was right before

--- js/crawler.js
+++ js/crawler.js
@@ -39,7 +39,7 @@ function findFiles(dir) {
 				} else {
 					if (!arrayContains(directories, url)) {
 						directories.push(url);
-						findFiles(dir + url);
+						findFiles("http://source.maxofs2d.net" + url);
 					}
 				}
 			}
