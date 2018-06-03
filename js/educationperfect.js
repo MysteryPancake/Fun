57f73dd9b7dacd8422ff8bfe8db28e67e287768c
MysteryPancake <MysteryPancake@users.noreply.github.com>
2018-06-03 21:36:11 +1000

    doesn't work

---
+++ js/educationperfect.js
@@ -0,0 +1,9 @@
+"use strict";
+
+setInterval(function() {
+	var answer = document.getElementsByClassName("field native-font ng-binding")[1].innerHTML;
+	if (!answer) {
+		document.getElementById("hint-button").click();
+	}
+	document.getElementById("answer-text").value = answer;
+}, 10);
