f34ea265104559c20246aabecffe9c5d5934a36d
MysteryPancake <MysteryPancake@users.noreply.github.com>
2018-06-03 21:39:18 +1000

    https://forum.facepunch.com/f/gmoddev/rutq/Replacement-Clientside-Killfeed/3/#postkoauq

---
+++ gmod/infront.lua
@@ -0,0 +1,3 @@
+local x, y = 20, 20
+local w = draw.SimpleText( "blah", "DermaDefault", x, y, Color( 255, 255, 255, 255 ) ) -- text 1
+draw.SimpleText( "in front", "DermaDefault", x + w, y, Color( 255, 0, 0, 255 ) ) -- text 2
