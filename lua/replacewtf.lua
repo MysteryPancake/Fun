b171f0d52cc7ed185b72861caf4548fafb5a4b82
MysteryPancake <MysteryPancake@users.noreply.github.com>
2018-06-03 21:24:57 +1000

    moved to Fun/gmod

--- lua/replacewtf.lua
+++
@@ -1,9 +0,0 @@
-hook.Add( "PlayerSay", "ReplaceWTF", function( ply, txt )
-    local tbl = string.Explode( " ", txt )
-    for k, v in ipairs( tbl ) do
-        if string.lower( v ) == "wtf" then
-            tbl[ k ] = "Wearing This Face"
-        end
-    end
-    return string.Implode( " ", tbl )
-end )
