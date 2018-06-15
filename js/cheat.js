c7f8a743f7682e36a1aa870076dd64092cd0cfa4
MysteryPancake <MysteryPancake@users.noreply.github.com>
2018-06-15 19:34:08 +1000

    good for "getting work done"

---
+++ js/cheat.js
@@ -0,0 +1,32 @@
+function clickButton(div) {
+	for (var i = 0; i < div.childNodes.length; i++) {
+		var node = div.childNodes[i];
+		if (node.tagName === "BUTTON") {
+			node.click();
+		}
+	}
+}
+
+function cheat() {
+	document.getElementsByClassName("long-answer-box")[0].innerHTML = "Not sure";
+	setTimeout(function() {
+		clickButton(document.getElementsByClassName("submit")[0]);
+		setTimeout(function() {
+			document.getElementsByClassName("star-4")[0].click();
+			setTimeout(function() {
+				clickButton(document.getElementsByClassName("edit")[0]);
+				setTimeout(function() {
+					var answer = document.getElementsByClassName("model-answer")[0].textContent.replace("Example Model Answer", "").trim();
+					document.getElementsByClassName("long-answer-box")[0].innerHTML = answer;
+				}, 500);
+			}, 500);
+		}, 500);
+	}, 500);
+}
+
+var menu = document.getElementsByClassName("sa-navigation-controls-content")[0];
+var button = document.createElement("BUTTON");
+var text = document.createTextNode("Cheat");
+button.appendChild(text);
+button.onclick = cheat;
+menu.appendChild(button);
