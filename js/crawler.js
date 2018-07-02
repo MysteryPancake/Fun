2d426d114cda7286f758fa904dda8fd7359b364b
MysteryPancake <MysteryPancake@users.noreply.github.com>
2018-07-02 16:55:35 +1000

    no more forEach

--- js/crawler.js
+++ js/crawler.js
@@ -4,16 +4,16 @@ const request = require("request");
 const cheerio = require("cheerio");
 
 const directories = [];
-const files = ["http://source.maxofs2d.net/rigs/3ds_2009/_WARNING_-_these_files_are_deprecated"];
-const ignore = ["mailto:ml.maxofs2d@gmail.com", "http://www.evoluted.net", "&sort=asc", "?dir=./"];
+const files = ["https://source.maxofs2d.net/rigs/3ds_2009/_WARNING_-_these_files_are_deprecated"];
+const ignore = ["mailto:ml.maxofs2d@gmail.com", "http://www.evoluted.net", "&sort=asc", "?dir=./", "https://www.youtube.com/channel/UC1umWYFZdqOrKuhR0ALig5A", "https://www.maxofs2d.net"];
 
 function arrayFind(array, word) {
 	let value = false;
-	array.forEach(function(str) {
-		if (word.indexOf(str) !== -1) {
+	for (let i = 0; i < array.length; i++) {
+		if (word.indexOf(array[i]) !== -1) {
 			value = true;
 		}
-	});
+	}
 	return value;
 }
 
@@ -27,24 +27,26 @@ function isFile(path) {
 
 function findFiles(dir) {
 	request(dir, function(error, response, body) {
-		const $ = cheerio.load(body);
-		$("a").each(function() {
-			const url = $(this).attr("href");
-			if (!arrayFind(ignore, url)) {
-				if (isFile(url)) {
-					if (!arrayContains(files, url)) {
-						files.push(url);
-						console.log(files.length, url);
-					}
-				} else {
-					if (!arrayContains(directories, url)) {
-						directories.push(url);
-						findFiles("http://source.maxofs2d.net" + url);
+		if (body) {
+			const $ = cheerio.load(body);
+			$("a").each(function() {
+				const url = $(this).attr("href");
+				if (!arrayFind(ignore, url)) {
+					if (isFile(url)) {
+						if (!arrayContains(files, url)) {
+							files.push(url);
+							console.log(files.length - 1, url);
+						}
+					} else {
+						if (!arrayContains(directories, url)) {
+							directories.push(url);
+							findFiles("https://source.maxofs2d.net" + url);
+						}
 					}
 				}
-			}
-		});
+			});
+		}
 	});
 }
 
-findFiles("http://source.maxofs2d.net"); // 249 files
+findFiles("https://source.maxofs2d.net"); // 249 files
