cb5baa3b94f25c69eea9ea83d0786d19472a00b8
MysteryPancake <MysteryPancake@users.noreply.github.com>
2018-07-08 22:31:03 +1000

    Update despacito.js

--- js/despacito.js
+++ js/despacito.js
@@ -2,6 +2,6 @@
 
 var images = document.getElementsByTagName("img");
 for (var i = 0; i < images.length; i++) {
-	var parent = images[i].parentElement;
+	var parent = images[i].parentNode;
 	parent.innerHTML = "<iframe width=\"" + parent.offsetWidth + "\" height=\"" + parent.offsetHeight + "\" src=\"https://www.youtube.com/embed/kJQP7kiw5Fk?autoplay=1&controls=0&loop=1&modestbranding=1&rel=0&showinfo=0\"></iframe>";
 }
