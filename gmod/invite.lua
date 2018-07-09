5fa8c9c2564e8abf9a4ce821f736848191d5995f
MysteryPancake <MysteryPancake@users.noreply.github.com>
2018-07-09 15:58:10 +1000

    Update invite.lua

--- gmod/invite.lua
+++ gmod/invite.lua
@@ -1,5 +1,5 @@
-hook.Add( "OnPlayerChat", "Discord", function( ply, txt )
-	if ply == LocalPlayer() and string.lower( txt ) == "!discord" then
+hook.Add( "OnPlayerChat", "Discord", function( ply, text )
+	if ply == LocalPlayer() and string.lower( text ) == "!discord" then
 		gui.OpenURL( "https://discordapp.com/invite/KruZzTv" )
 		return true
 	end
