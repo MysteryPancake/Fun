766abd50bae387f0289be4ad9a15e7ef7dbc08aa
MysteryPancake <MysteryPancake@users.noreply.github.com>
2019-07-17 17:49:54 +1000

    actually works now holy dooley

--- jsx/clearvid.jsx
+++ jsx/clearvid.jsx
@@ -1,26 +1,54 @@
 {
+	function convex(points, pointLookup) {
+		var verts = [];
+		for (var i = 0; i < points.length; i++) {
+			var x = points[i][0];
+			var y = points[i][1];
+			if (pointLookup[(x + 1) + ", " + y] && pointLookup[(x - 1) + ", " + y] && pointLookup[x + ", " + (y + 1)] && pointLookup[x + ", " + (y - 1)]) continue;
+			verts.push(points[i]);
+		}
+		verts.sort(function(a, b) {
+			return a[1] - b[1]
+		});
+		var centerY = (verts[0][1] + verts[verts.length - 1][1]) / 2;
+		verts.sort(function(a, b) {
+			return b[0] - a[0]
+		});
+		var centerX = (verts[0][0] + verts[verts.length - 1][0]) / 2;
+		var center = { x: centerX, y: centerY };
+		verts.sort(function(a, b) {
+			return Math.atan2(a[1] - center.y, a[0] - center.x) - Math.atan2(b[1] - center.y, b[0] - center.x);
+		});
+		return verts;
+	}
+
 	function parse(txt, layer, comp) {
+		//var roto = layer.property("Effects").addProperty("Roto Brush & Refine Edge");
 		var mask = layer.property("Masks").addProperty("Mask");
 		var path = mask.property("Mask Path");
 		var lines = txt.split("\n");
-		while (lines.length) {
+		while (lines.length > 0) {
 			var line = lines.shift();
 			if (line.indexOf("frame: ") !== -1) {
 				var frame = parseInt(line.split(": ").pop());
-				lines.shift();
-				var coords = lines.shift().split(",");
-				var verts = [];
-				while (coords.length) {
-					var x = parseInt(coords.shift());
-					var y = parseInt(coords.shift());
-					if (x !== null && y !== null) {
-						verts.push([x, y]);
+				if (!isNaN(frame)) {
+					lines.shift();
+					var points = [];
+					var pointLookup = {};
+					var coords = lines.shift().split(",");
+					for (var i = 0; i < coords.length; i += 2) {
+						var y = parseInt(coords[i]);
+						var x = parseInt(coords[i + 1]);
+						if (!isNaN(x) && !isNaN(y)) {
+							points.push([x, y]);
+							pointLookup[x + ", " + y] = true;
+						}
 					}
+					var maskShape = new Shape();
+					maskShape.vertices = convex(points, pointLookup);
+					maskShape.closed = true;
+					path.setValueAtTime(frame * comp.frameDuration, maskShape);
 				}
-				var maskShape = new Shape();
-				maskShape.vertices = verts;
-				maskShape.closed = true;
-				path.setValueAtTime(frame * comp.frameDuration, maskShape);
 			}
 		}
 	}
