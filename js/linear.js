7981b6f5028f08e0a1bf97315091fb4db78e7fb2
MysteryPancake <MysteryPancake@users.noreply.github.com>
2019-09-28 10:46:23 +1000

    Update linear.js

--- js/linear.js
+++ js/linear.js
@@ -1,4 +1,4 @@
-function limitlessLinear(t, tMin, tMax, value1, value2) {
+function limitlessLinear(t, tMin, tMax, value1, value2) { // AKA lerp
 	var normal = (t - tMin) / (tMax - tMin);
 	return (value1 * (1 - normal)) + (value2 * normal);
 }
