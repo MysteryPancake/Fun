d08f65b9ef8e4c7d5ca8090046812509d4919d71
MysteryPancake <MysteryPancake@users.noreply.github.com>
2020-05-07 01:19:01 +1000

    Update success.js

--- js/success.js
+++ js/success.js
@@ -1,5 +1,5 @@
 var bars = document.getElementsByClassName("progress-bar progress-bar-success");
 for (var i = 0; i < bars.length; i++) {
 	bars[i].style.width = "100%";
-	bars[i].innerHTML = 100;
+	bars[i].textContent = 100;
 }
