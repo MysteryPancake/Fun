cea1b5f2083bcc07d86fabb774388c3b013896d5
MysteryPancake <MysteryPancake@users.noreply.github.com>
2020-05-07 01:16:55 +1000

    Update cheat.js

--- js/cheat.js
+++ js/cheat.js
@@ -8,7 +8,7 @@ function clickButton(div) {
 }
 
 function cheat() {
-	document.getElementsByClassName("long-answer-box")[0].innerHTML = "Not sure";
+	document.getElementsByClassName("long-answer-box")[0].textContent = "Not sure";
 	window.setTimeout(function() {
 		clickButton(document.getElementsByClassName("submit")[0]);
 		window.setTimeout(function() {
@@ -17,9 +17,9 @@ function cheat() {
 				clickButton(document.getElementsByClassName("edit")[0]);
 				window.setTimeout(function() {
 					var answer = document.getElementsByClassName("model-answer")[0].textContent.replace("Example Model Answer", "").trim();
-					document.getElementsByClassName("long-answer-box")[0].innerHTML = answer;
+					document.getElementsByClassName("long-answer-box")[0].textContent = answer;
 					var keywords = document.getElementsByClassName("answer-keyword")[0].textContent.replace("Keywords", "").trim();
-					document.getElementsByClassName("long-answer-input")[0].innerHTML += "Keywords: " + keywords;
+					document.getElementsByClassName("long-answer-input")[0].textContent += "Keywords: " + keywords;
 				}, 500);
 			}, 500);
 		}, 500);
@@ -28,6 +28,6 @@ function cheat() {
 
 var menu = document.getElementsByClassName("sa-navigation-controls-content")[0];
 var button = document.createElement("BUTTON");
-button.innerHTML = "Cheat";
+button.textContent = "Cheat";
 button.onclick = cheat;
 menu.appendChild(button);
