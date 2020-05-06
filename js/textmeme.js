0ff772ecabd9437f9f7c02773bbb03b840e19ec5
MysteryPancake <MysteryPancake@users.noreply.github.com>
2020-05-07 01:18:31 +1000

    Update textmeme.js

--- js/textmeme.js
+++ js/textmeme.js
@@ -1,5 +1,5 @@
 var elems = document.querySelectorAll("a,time,h1,p");
 
 setInterval(function() {
-	elems[Math.floor(Math.random()*elems.length)].innerHTML = "Sample Text";
+	elems[Math.floor(Math.random()*elems.length)].textContent = "Sample Text";
 }, 100);
