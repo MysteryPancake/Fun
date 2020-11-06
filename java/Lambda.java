0a94265f163e977a61d40f1de9c58e3817e5bf14
MysteryPancake <MysteryPancake@users.noreply.github.com>
2020-11-06 22:36:14 +1100

    Add files via upload

---
+++ java/Lambda.java
@@ -0,0 +1,22 @@
+public class Lambda {
+
+	private static int i = 0;
+
+	interface StringBuilder {
+		String run(String s);
+	}
+
+	public Lambda() {
+		System.out.println(this);
+	}
+
+	public String toString() {
+		StringBuilder e = (i) -> this + i;
+		if (i > 10) return "bruh";
+		return e.run(String.valueOf(i++));
+	}
+
+	public static void main(String[] args) {
+		new Lambda();
+	}
+}
