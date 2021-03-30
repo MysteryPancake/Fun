fb3496156bdedbc2407aad5731f198fe31ef6ada
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-03-30 17:21:01 +1100

    Add files via upload

---
+++ java/ordchar.java
@@ -0,0 +1,11 @@
+class OrdChar {
+
+	public static void main(String[] args) {
+		
+		// ord() = cast char to int
+		System.out.println((int)'h');
+
+		// chr() = cast int to char
+		System.out.println((char)104);
+	}
+}
