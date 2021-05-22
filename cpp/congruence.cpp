23c66af9cc6547ed43d6b44108a5f397538426f3
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-05-22 16:01:15 +1000

    Add files via upload

---
+++ cpp/congruence.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << (int) -21474794411 << '\n';
+	std::cout << (int) -17179827115 << '\n';
+	std::cout << (int) -12884859819 << '\n';
+	std::cout << (int) -8589892523 << '\n';
+	std::cout << (int) -4294925227 << '\n';
+	std::cout << (int) 4295009365 << '\n';
+	std::cout << (int) 8589976661 << '\n';
+	std::cout << (int) 12884943957 << '\n';
+	std::cout << (int) 17179911253 << '\n';
+	std::cout << (int) 21474878549 << '\n';
+	std::cout << (int) 25769845845 << '\n';
+
+	return 0;
+}
