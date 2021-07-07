991f45cdbd163dd627f2c3ab52625dbd72d52023
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-07-07 19:33:53 +1000

    updated to match macOS version

--- batch/youtubedl.bat
+++ batch/youtubedl.bat
@@ -2,30 +2,73 @@
 
 :loop
 
-set /p link=LINK: 
+echo.
+echo VIDEO WITH AUDIO
+echo  1. YouTube to MP4 (Recommended)
+echo  2. Choose me if option 1 fails
+echo  3. Choose me if option 2 fails
+echo.
+echo AUDIO ONLY
+echo  4. YouTube to WAV
+echo  5. SoundCloud
+echo.
+echo ARCHIVE
+echo  6. YouTube to MP4 + Metadata
+echo  7. Choose me if option 6 fails
+echo  8. Choose me if option 7 fails
+echo.
+echo If everything fails, update YouTube-DL with youtube-dl -U
+echo.
+set /p option=Please choose an option: 
+echo.
 
-:: FOR SOUNDCLOUD:
-:: youtube-dl.exe -f bestaudio --audio-format best --add-metadata --embed-thumbnail %link%
+if %option% lss 1 goto INVALID
+if %option% gtr 8 goto INVALID
 
-:: BAD FOR YOUTUBE:
-:: youtube-dl.exe -f best --add-metadata %link%
+set /p link=Please paste a link: 
 
-:: FOR YOUTUBE:
-:: youtube-dl.exe -f bestvideo+bestaudio --add-metadata %link%
+goto OPTION%option%
 
-:: FOR YOUTUBE WAV:
-:: youtube-dl.exe -f bestaudio -x --audio-format wav --add-metadata %link%
+:OPTION1
+:: YOUTUBE MP4 BEST QUALITY
+youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 --add-metadata %link%
+goto loop
 
-:: FOR YOUTUBE MP4:
-youtube-dl.exe -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 --add-metadata %link%
+:OPTION2
+:: YOUTUBE MP4 DECENT QUALITY
+youtube-dl -f bestvideo+bestaudio --add-metadata %link%
+goto loop
 
-:: BAD FOR YOUTUBE WITH DATA:
-:: youtube-dl.exe -f best --write-thumbnail --write-description --write-info-json --write-annotations --add-metadata %link%
+:OPTION3
+:: YOUTUBE MP4 BAD QUALITY
+youtube-dl -f best --add-metadata %link%
+goto loop
 
-:: FOR YOUTUBE WITH DATA:
-:: youtube-dl.exe -f bestvideo+bestaudio --write-thumbnail --write-description --write-info-json --write-annotations --add-metadata %link%
+:OPTION4
+:: YOUTUBE WAV
+youtube-dl -f bestaudio -x --audio-format wav --add-metadata %link%
+goto loop
 
-:: FOR YOUTUBE MP4 WITH DATA:
-:: youtube-dl.exe -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 --write-thumbnail --write-description --write-info-json --write-annotations --add-metadata %link%
+:OPTION5
+:: SOUNDCLOUD
+youtube-dl -f bestaudio --audio-format best --add-metadata --embed-thumbnail %link%
+goto loop
 
+:OPTION6
+:: YOUTUBE MP4 BEST QUALITY WITH DATA
+youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 --write-thumbnail --write-description --write-info-json --write-annotations --add-metadata %link%
+goto loop
+
+:OPTION7
+:: YOUTUBE MP4 DECENT QUALITY WITH DATA
+youtube-dl -f bestvideo+bestaudio --write-thumbnail --write-description --write-info-json --write-annotations --add-metadata %link%
+goto loop
+
+:OPTION8
+:: YOUTUBE MP4 BAD QUALITY WITH DATA
+youtube-dl -f best --write-thumbnail --write-description --write-info-json --write-annotations --add-metadata %link%
+goto loop
+
+:INVALID
+echo Invalid option, please try again.
 goto loop
