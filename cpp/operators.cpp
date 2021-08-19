974cacd8cda4d43c7801af07782b4180bff49c3d
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-08-19 19:52:11 +1000

    Add files via upload

---
+++ cpp/operators.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#define lower(a) (a >= 'A' && a <= 'Z' ? (char)(a + 32) : a)
+#define alpha(a) ((a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'z'))
+
+int main()
+{
+    std::cout << lower('H') << lower('i') << lower('B') << lower('R') << lower('U') << lower('H');
+    std::cout << alpha('H') << alpha('a') << alpha('?');
+}
