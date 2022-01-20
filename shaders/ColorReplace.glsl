a78424d7bb65510cb25842b4882e93fdf1bc7c47
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-01-20 21:51:29 +1100

    Add files via upload

--- shaders/ColorReplace.glsl
+++ shaders/ColorReplace.glsl
@@ -4,10 +4,10 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
   vec4 col = texture(iChannel0, fragCoord / iResolution.xy);
   
   vec3 target = vec3(0.0, 0.0, 0.0); // Find black
-  vec3 replace = vec3(0.0, 1.0, 0.0); // Replace with green
+  vec3 replace = vec3(1.0, 0.0, 0.0); // Replace with red
   
-  float threshold = 0.5; // Controls target color range
-  float softness = 0.2; // Controls soft falloff
+  float threshold = 0.25; // Controls target color range
+  float softness = 0.25; // Controls soft falloff
   
   // Get difference to use for falloff if required
   float diff = distance(col.xyz, target.xyz) - threshold;
