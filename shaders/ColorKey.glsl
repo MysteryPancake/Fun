915e5fad4c9153c074d22d4ce0278fb1c29909e9
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-01-20 21:52:39 +1100

    Add files via upload

---
+++ shaders/ColorKey.glsl
@@ -0,0 +1,19 @@
+// Available at https://www.shadertoy.com/view/NsfcWj
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+
+  vec4 col = texture(iChannel0, fragCoord / iResolution.xy);
+  vec4 replace = texture(iChannel1, fragCoord / iResolution.xy);
+  
+  vec3 target = vec3(0.0, 1.0, 0.0); // Find green
+  
+  float threshold = 0.5; // Controls target color range
+  float softness = 0.25; // Controls soft falloff
+  
+  // Get difference to use for falloff if required
+  float diff = distance(col.xyz, target.xyz) - threshold;
+  
+  // Apply soft falloff if needed, otherwise clamp
+  float factor = clamp(diff / softness, 0.0, 1.0);
+  
+  fragColor = vec4(mix(replace.xyz, col.xyz, factor), col.a);
+}
