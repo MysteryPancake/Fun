c512ee34dea69b21c79e926572aedbf85e46c9d5
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-08-11 17:54:01 +1000

    Add files via upload

--- shaders/MosaicGlass.glsl
+++ shaders/MosaicGlass.glsl
@@ -1,3 +1,4 @@
+// Available at https://www.shadertoy.com/view/ft3czf
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
 	vec2 uv = fragCoord / iResolution.xy;
 	float pixels = mod(iTime * 10.0, 50.0);
