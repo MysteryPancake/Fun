0ad6fe5548771c81cca3156b70c1fed729cfd8bf
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-08-20 20:08:42 +1000

    Add files via upload

--- shaders/CircleMan.glsl
+++ shaders/CircleMan.glsl
@@ -7,20 +7,18 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
 	const float threshold = 0.5; // Controls target color range
 	const float softness = 0.1; // Controls linear falloff
 
-	const int steps = 64;
-	const float total = float(steps) / 6.28318530;
-	
+	const float steps = 64.0;
+	const float total = steps / 6.28318530;
+
 	fragColor = texture(iChannel0, uv);
-	
-	for (int i = 0; i < steps; i++) {
+
+	for (float i = 0.0; i < steps; i++) {
 		// Sample image in a circular pattern
-		float j = float(i) / total;
+		float j = i / total;
 		vec4 col = texture(iChannel0, uv + vec2(sin(j), cos(j)) * 0.1);
 		
-		// Get difference to use for falloff if required
+		// Color key
 		float diff = distance(col.xyz, target) - threshold;
-		
-		// Apply linear falloff if needed, otherwise clamp
 		fragColor = max(fragColor, col * clamp(diff / softness, 0.0, 1.0));
 	}
 }
