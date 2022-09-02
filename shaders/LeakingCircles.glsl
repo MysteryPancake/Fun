b3e76b0794ff1306df436d731a6b708ca866d2e7
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-09-02 19:34:02 +1000

    Add files via upload

---
+++ shaders/LeakingCircles.glsl
@@ -0,0 +1,14 @@
+// Available at https://www.shadertoy.com/view/7lycWy
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+
+	vec2 uv = fragCoord / iResolution.xy;
+	vec2 noDistort = fragCoord / iResolution.x;
+	
+	float grid = distance(fract(noDistort * 14.0), vec2(0.5));
+	float radius = iTime * 0.03;
+	float falloff = radius / distance(uv, vec2(0.5));
+	float blurred = smoothstep(0.4, 0.6, falloff - grid);
+	
+	vec3 color = mix(vec3(uv.x, 0.0, uv.y), vec3(1.0), blurred);
+	fragColor = vec4(color, 1.0);
+}
