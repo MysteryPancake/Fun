795491bee4ea7200f8835f6c83cf97b485e1bc85
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-10-06 02:51:44 +1100

    Update ColorReplace.glsl

--- glsl/ColorReplace.glsl
+++ glsl/ColorReplace.glsl
@@ -3,17 +3,17 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
 
   vec4 col = texture(iChannel0, fragCoord / iResolution.xy);
   
-  vec3 target = vec3(0.0, 0.0, 0.0); // Find black
-  vec3 replace = vec3(1.0, 0.0, 0.0); // Replace with red
+  const vec3 target = vec3(0.0, 0.0, 0.0); // Find black
+  const vec3 replace = vec3(1.0, 0.0, 0.0); // Replace with red
   
-  float threshold = 0.25; // Controls target color range
-  float softness = 0.25; // Controls linear falloff
+  const float threshold = 0.25; // Controls target color range
+  const float softness = 0.25; // Controls linear falloff
   
   // Get difference to use for falloff if required
-  float diff = distance(col.xyz, target) - threshold;
+  float diff = distance(col.rgb, target) - threshold;
   
   // Apply linear falloff if needed, otherwise clamp
   float factor = clamp(diff / softness, 0.0, 1.0);
   
-  fragColor = vec4(mix(replace, col.xyz, factor), col.a);
+  fragColor = vec4(mix(replace, col.rgb, factor), col.a);
 }
