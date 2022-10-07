62ecdaa698844b84416f5d8f0185f5d9066512e2
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-10-07 21:18:10 +1100

    Add files via upload

--- glsl/CellularMaze.glsl
+++ glsl/CellularMaze.glsl
@@ -4,9 +4,9 @@
 
 #define gridSize 8.0
 
-#define cell(offset) texelFetch(iChannel0, ivec2(fragCoord) + offset, 0).x
-
 // BUFFER A
+
+#define cell(offset) texelFetch(iChannel0, ivec2(fragCoord) + offset, 0).x
 
 float rand(vec2 p) {
 	return fract(sin(dot(p, vec2(12.9898, 78.233))) * 43758.5453);
