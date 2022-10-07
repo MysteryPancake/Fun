ca06a9aa6d7b945be3dbf5b03f4e587a7c6b5e05
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-10-08 02:23:30 +1100

    Add files via upload

--- glsl/CellularMaze.glsl
+++ glsl/CellularMaze.glsl
@@ -8,6 +8,8 @@
 
 #define cell(offset) texelFetch(iChannel0, ivec2(fragCoord) + offset, 0).x
 
+#define resetAfter 64
+
 float rand(vec2 p) {
 	return fract(sin(dot(p, vec2(12.9898, 78.233))) * 43758.5453);
 }
@@ -17,7 +19,7 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
 	// Skip invisible pixels
 	if (fragCoord.x * gridSize > iResolution.x || fragCoord.y * gridSize > iResolution.y) discard;
 
-	if (iFrame % 64 == 0) {
+	if (iFrame % resetAfter == 0) {
 	
 		// Initialize values, random noise
 		fragColor = vec4(vec3(rand(fragCoord / iResolution.xy)), 1.0);
@@ -31,22 +33,15 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
 		float sum = cell(ivec2(-1,  1)) + cell(ivec2(0,  1)) + cell(ivec2(1,  1))
 				  + cell(ivec2(-1,  0))                      + cell(ivec2(1,  0))
 				  + cell(ivec2(-1, -1)) + cell(ivec2(0, -1)) + cell(ivec2(1, -1));
-
+				 
 		// Swap pattern every 64 frames
-		float offset = sum > float((64 + iFrame) / 64 % 8) ? -0.1 : 0.1;
+		float offset = sum > float((1 + iFrame / resetAfter) % 8) ? -0.1 : 0.1;
 		fragColor = vec4(vec3(clamp(self + offset, 0.0, 1.0)), 1.0);
 	}
 }
 
 // IMAGE
 
-#define SHARP
-
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
-
-	#ifdef SHARP
-		fragColor = texelFetch(iChannel0, ivec2(fragCoord / gridSize), 0);
-	#else
-		fragColor = texture(iChannel0, fragCoord / iResolution.xy / gridSize);
-	#endif
+	fragColor = texelFetch(iChannel0, ivec2(fragCoord / gridSize), 0);
 }
