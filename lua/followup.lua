0ff5e8571ba5a29aa39d3432994856529e2aa567
shawnjbragdon <115764403+shawnjbragdon@users.noreply.github.com>
2022-10-13 20:34:19 -0700

    Include type assertion in lua/followup.lua

--- lua/followup.lua
+++ lua/followup.lua
@@ -1,52 +1,53 @@
 local strtab = {}
 
-local function AddFollowUp( str ) -- adds a followup thing to the word table, e.g. I ate > ate is a followup word of I
-	str = string.lower( str )
-	str = str:gsub( '[.,?!“"]', "" ) -- remove full stops and question marks and all that
-	local strs = {}
-	for word in str:gmatch( "%S+" ) do
-		strs[ #strs + 1 ] = word
+local function AddFollowUp(str) -- adds a followup thing to the word table, e.g. I ate > ate is a followup word of I
+    assert(type(str) == "string", "Invalid argument #1 for AddFollowUp (string expected)")
+    str = str:lower():gsub('[.,?!“"]', "") -- remove full stops and question marks and all that
+    local strs = {}
+	for word in str:gmatch("%S+") do
+		strs[#strs + 1] = word
 	end
-	for k, v in ipairs( strs ) do
-		local nextkey = strs[ k + 1 ]
+	for k, v in ipairs(strs) do
+		local nextkey = strs[k + 1]
 		if nextkey then
-			if not strtab[ v ] then
-				strtab[ v ] = {}
+			if not strtab[v] then
+				strtab[v] = {}
 			end
-			strtab[ v ][ #strtab[ v ] + 1 ] = nextkey
+			strtab[v][#strtab[v] + 1] = nextkey
 		end
 	end
 end
 
-local function RandomKey( tab ) -- Gets a random key of a table
+local function RandomKey(tab) -- Gets a random key of a table
 	local t = {}
-	for k,v in pairs( tab ) do
-		t[ #t + 1 ] = k
+	for k, v in pairs(tab) do
+		t[#t + 1] = k
 	end
-	return t[ math.random( #t ) ]
+	return t[math.random(#t)]
 end
 
-local function CapitalizeFirst( str ) -- Capitalizes the first letter of a string
-	return ( str:gsub( "^%l", string.upper ) )
+local function CapitalizeFirst(str) -- Capitalizes the first letter of a string
+    assert(type(str) == "string", "Invalid argument #1 for CapitalizeFirst (string expected)")
+	return (str:gsub("^%l", string.upper))
 end
 
-local function GenerateFollowupString( length ) -- Makes a string out of words added from AddFollowUp
+local function GenerateFollowupString(length) -- Makes a string out of words added from AddFollowUp
 	local s = ""
 	local prevword
 	for i = 1, length do
-		local randomword = RandomKey( strtab )
+		local randomword = RandomKey(strtab)
 		if prevword then
-			local followupword = strtab[ prevword ]
-			if followupword and next( followupword ) ~= nil then
-				local randomwordfromtab = followupword[ math.random( #followupword ) ]
+			local followupword = strtab[prevword]
+			if followupword and next(followupword) ~= nil then
+				local randomwordfromtab = followupword[math.random(#followupword)]
 				s = s .. " " .. randomwordfromtab
 				prevword = randomwordfromtab
 			else
-				s = s .. ". " .. CapitalizeFirst( randomword )
+				s = s .. ". " .. CapitalizeFirst(randomword)
 				prevword = randomword
 			end
 		else
-			s = s .. CapitalizeFirst( randomword )
+			s = s .. CapitalizeFirst(randomword)
 			prevword = randomword
 		end
 	end
