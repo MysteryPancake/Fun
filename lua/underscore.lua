fbc9ecde63350f60dc1445f91c96b69af3cd3c15
shawnjbragdon <115764403+shawnjbragdon@users.noreply.github.com>
2022-10-13 20:39:07 -0700

    Update underscore.lua

--- lua/underscore.lua
+++ lua/underscore.lua
@@ -1,4 +1,13 @@
--- Don't do this:
-local blah, _ = SomeFunction()
--- Do this:
-local blah = SomeFunction()
+--- If a function returns several values, but you only need it's second value, skip the first with an underscore.
+
+local _, variable = myFunction()
+
+--- You can use select to avoid this as well.
+
+local variable = select(2, myFunction())
+
+--- This is often seen in loops with unused index variables.
+
+for _, v in pairs({}) do
+    print(tostring(v))
+end
