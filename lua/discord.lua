f0e0375b7e6df0f05a544fd9215d59a9fc4c8bc3
shawnjbragdon <115764403+shawnjbragdon@users.noreply.github.com>
2022-10-13 20:31:10 -0700

    Minify lua/discord.lua
    Faster loading times.

--- lua/discord.lua
+++ lua/discord.lua
@@ -1,26 +1,14 @@
 local str = "22 Jump Street is the best movie ever made"
-
-local numbers = {
-	[ 0 ] = ":zero: ",
-	[ 1 ] = ":one: ",
-	[ 2 ] = ":two: ",
-	[ 3 ] = ":three: ",
-	[ 4 ] = ":four: ",
-	[ 5 ] = ":five: ",
-	[ 6 ] = ":six: ",
-	[ 7 ] = ":seven: ",
-	[ 8 ] = ":eight: ",
-	[ 9 ] = ":nine: "
-}
+local numbers = { [0] = ":zero: ", ":one: ", ":two: ", ":three: ", ":four: ", ":five: ", ":six: ", ":seven: ", ":eight: ", ":nine: " }
 
 for i = 1, #str do
-	local letter = string.lower( string.sub( str, i, i ) )
-	local number = tonumber( letter )
-	if number then
-		io.write( numbers[ number ] )
+	local letter = string.lower(string.sub(str, i, i))
+	local number = select(2, tonumber, letter)
+	if type(number) == "number" then
+		io.write(numbers[number])
 	elseif letter == " " then
-		io.write( " " )
+		io.write(" ")
 	else
-		io.write( ":regional_indicator_" .. letter .. ": " )
-	end	
+		io.write(":regional_indicator_" .. letter .. ": ")
+	end
 end
