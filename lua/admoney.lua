1c19c4cbe5d3771bb3cb4aec8e2737ca3e7a126b
shawnjbragdon <115764403+shawnjbragdon@users.noreply.github.com>
2022-10-13 20:28:36 -0700

    Remove unnecessary variables in lua/admoney.lua

--- lua/admoney.lua
+++ lua/admoney.lua
@@ -1,7 +1,5 @@
 local lengthMinutes = 20
 
 for i = 2, 60 * lengthMinutes do
-	local minute = math.floor( i / 60 )
-	local second = string.format( "%02.f", i % 60 )
-	io.write( minute .. ":" .. second .. ", " )
+	io.write(tostring(math.floor(i / 60)) .. ":" .. tostring(string.format("%02.f", i % 60)) .. ", ")
 end
