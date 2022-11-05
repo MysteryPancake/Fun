e39adc8bbb885dd9bf138523bc55716880e2d85e
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-11-06 06:36:27 +1100

    Add files via upload

---
+++ glsl/HallOfMirrors.glsl
@@ -0,0 +1,60 @@
+// Available at https://www.shadertoy.com/view/dsSGWm
+
+// Faithful remake of the deleted "Hall of Mirrors" effect by Red Giant
+// Recreated by MysteryPancake
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+
+	// Number of images to display
+	int images = 10;
+	// Scale factor per image
+	float scale = 0.8 + cos(iTime * 2.0) * 0.05;
+	// Rotation per image in degrees
+	float rotation = sin(iTime * 0.5) * 90.0;
+	// Position offset per image in normalized coordinates (0-1)
+	vec2 offset = iMouse.z > 0.0 ? vec2(iMouse.xy / iResolution.xy) : 0.5 + vec2(cos(iTime), sin(iTime)) * 0.25;
+	// Composite behind or in front
+	bool behind = false;
+
+	float rad = radians(-rotation);
+	vec2 uv = fragCoord / iResolution.xy;
+	fragColor = vec4(0.0);
+
+	for (int i = 0; i < images; ++i) {
+		// Avoid divide by zero error
+		if (scale <= 0.0) {
+			fragColor = texture(iChannel0, uv);
+			break;
+		}
+
+		// SCALING: Offset to center, apply scale, reset offset
+		vec2 pos = uv - offset;
+		pos /= pow(scale, float(i));
+		pos += offset;
+
+		// ROTATION
+		float theta = rad * float(i);
+		float cs = cos(theta);
+		float sn = sin(theta);
+		// Offset to center, fix aspect ratio
+		pos -= vec2(0.5);
+		pos *= iResolution.xy;
+		// Rotate coordinate space
+		pos = vec2(pos.x * cs - pos.y * sn, pos.x * sn + pos.y * cs);
+		// Reset aspect ratio, reset offset
+		pos /= iResolution.xy;
+		pos += vec2(0.5);
+
+		// Prevent out of bounds bugs, could also be done with clamp
+		if (pos.x >= 0.0 && pos.x <= 1.0 && pos.y >= 0.0 && pos.y <= 1.0) {
+			vec4 color = texture(iChannel0, pos);
+			if (behind) {
+				fragColor = fragColor + (1.0 - fragColor.a) * color;
+			} else {
+				fragColor = color + (1.0 - color.a) * fragColor;
+			}
+		}
+	}
+	// Convert premultiplied alpha to straight alpha
+	fragColor.rgb /= fragColor.a;
+}
