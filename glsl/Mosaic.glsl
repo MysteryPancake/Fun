3f1cc3f0a7f4536f3ee3144fda977a51242ab5b4
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-12-30 00:18:41 +1100

    Add files via upload

--- glsl/Mosaic.glsl
+++ glsl/Mosaic.glsl
@@ -1,6 +1,7 @@
 // Available at https://www.shadertoy.com/view/stdcz2
+
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
-	vec2 uv = fragCoord / iResolution.xy;
-	float pixels = mod(iTime * 10.0, 50.0);
-	fragColor = textureLod(iChannel0, floor(uv * pixels) / pixels, 0.0);
+	float pixels = mod(iTime * 10., 50.);
+	vec2 uv = floor(fragCoord / iResolution.xy * pixels) / pixels;
+	fragColor = textureLod(iChannel0, uv, 0.);
 }
