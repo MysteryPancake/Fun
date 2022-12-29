3e79760dadbf5a09994e8610bba14b90e3d1f86f
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-12-30 02:53:34 +1100

    Add files via upload

--- glsl/ShortColorKey.glsl
+++ glsl/ShortColorKey.glsl
@@ -1,15 +1,15 @@
 // Available at https://www.shadertoy.com/view/dtXGDB
 
-// Sharp alpha, 111 chars (thanks Alpgaga)
+// Sharp alpha, 109 chars (thanks Alpgaga, kishimisu)
 void mainImage(out vec4 O, vec2 u) {
 	O = texture(iChannel0, u / iResolution.xy);
-	O -= step(length(O.rgb - vec3(0, 1, 0)), .7);
+	O -= step(length(O - vec4(0, 1, 0, 1)), .7);
 }
 
-// Smooth alpha, 120 chars
+// Smooth alpha, 118 chars
 /*void mainImage(out vec4 O, vec2 u) {
 	O = texture(iChannel0, u / iResolution.xy);
-	O *= smoothstep(.5, .7, length(O.rgb - vec3(0, 1, 0)));
+	O *= smoothstep(.5, .7, length(O - vec4(0, 1, 0, 1)));
 }*/
 
 // Terrible alpha, 89 chars
