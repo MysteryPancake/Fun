4468493aac17bd9561136c9fc668545692c5bf7a
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-12-30 13:24:01 +1100

    Add files via upload

--- glsl/ShortColorKey.glsl
+++ glsl/ShortColorKey.glsl
@@ -2,25 +2,25 @@
 
 // By FabriceNeyret2, 105 chars :O
 void mainImage(out vec4 O, vec2 u) {
-    vec4 G = O = texture(iChannel0, u / iResolution.xy);
-    G.ga--;
-    dot(G, G) < .5 ? O-- : O;
+	vec4 G = O = texture(iChannel0, u / iResolution.xy);
+	G.ga--;
+	dot(G, G) < .5 ? O-- : O;
 }
 
 // Sharp alpha, 109 chars (thanks Alpgaga, kishimisu)
 /*void mainImage(out vec4 O, vec2 u) {
-    O = texture(iChannel0, u / iResolution.xy);
-    O -= step(length(O - vec4(0, 1, 0, 1)), .7);
+	O = texture(iChannel0, u / iResolution.xy);
+	O -= step(length(O - vec4(0, 1, 0, 1)), .7);
 }*/
 
 // Smooth alpha, 118 chars
 /*void mainImage(out vec4 O, vec2 u) {
-    O = texture(iChannel0, u / iResolution.xy);
-    O *= smoothstep(.5, .7, length(O - vec4(0, 1, 0, 1)));
+	O = texture(iChannel0, u / iResolution.xy);
+	O *= smoothstep(.5, .7, length(O - vec4(0, 1, 0, 1)));
 }*/
 
 // Terrible alpha, 89 chars
 /*void mainImage(out vec4 O, vec2 u) {
-    O = texture(iChannel0, u / iResolution.xy);
-    O -= step(O.r, .1);
+	O = texture(iChannel0, u / iResolution.xy);
+	O -= step(O.r, .1);
 }*/
