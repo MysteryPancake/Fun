7f0bbc2923220b6123224be2e165b862da813105
MysteryPancake <MysteryPancake@users.noreply.github.com>
2023-01-20 02:11:50 +1100

    Add files via upload

---
+++ glsl/ClaudeWide.glsl
@@ -0,0 +1,74 @@
+// Available at https://www.shadertoy.com/view/mlS3Dd
+
+const float TAU = 6.28318530;
+
+float getAlpha(vec3 col) {
+	const vec3 green = vec3(0.0, 1.0, 0.0);
+	return step(0.7, distance(col, green));
+}
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+
+	const float dirSteps = 64.0;
+	const float radius = 32.0;
+	
+	// Distortion factor, change with mouse
+	float distort = iMouse.z > 0.0 ? iMouse.x / iResolution.x * 2.0 - 1.0 : -0.5;
+	
+	vec2 uv = fragCoord / iResolution.xy;
+	vec3 bg = vec3(uv.x, 1.0 - uv.y, 1.0) + smoothstep(0.4, 0.6, length(uv - 0.5));
+	
+	fragColor = texture(iChannel0, uv);
+	float alpha = getAlpha(fragColor.rgb);
+	
+	// Correct aspect ratio
+	vec2 aspect = 1.0 / vec2(textureSize(iChannel0, 0));
+	
+	// STEP 1: Find average direction away from edge
+	
+	vec2 dirAvg = vec2(0.0);
+	float total = 0.0;
+	bool interior = false;
+	
+	for (float i = 0.0; i < TAU; i += TAU / dirSteps) {
+		// Move in a circle to find edges within radius
+		vec2 dir = vec2(sin(i), cos(i));
+		vec4 col = texture(iChannel0, uv + dir * aspect * radius);
+		
+		// Edge was found, accumulate for average
+		float neighbor = getAlpha(col.rgb);
+		if (alpha != neighbor) {
+			dirAvg += dir;
+			total++;
+			interior = alpha > neighbor;
+		}
+	}
+	
+	// No edges within radius
+	if (total <= 0.0) {
+		fragColor.rgb = mix(bg, fragColor.rgb, alpha);
+		return;
+	}
+	
+	// Calculate average direction
+	dirAvg = normalize(dirAvg / total);
+
+	// STEP 2: Raycast in average direction until an edge is hit
+	
+	float d = radius * 0.5;
+	float move = d * 0.5;
+	// Binary search works better than linear search
+	for (int i = 0; i < 8; i++, move *= 0.5) {
+		vec4 col = texture(iChannel0, uv + dirAvg * aspect * d);
+		d += alpha != getAlpha(col.rgb) ? -move : move;
+	}
+	
+	// STEP 3: Fix interior direction and distance
+	
+	dirAvg = interior ? dirAvg : -dirAvg;
+	d = interior ? radius - d : d + radius;
+
+	// Distort the image
+	fragColor = texture(iChannel0, uv + dirAvg * aspect * d * distort);
+	fragColor.rgb = mix(bg, fragColor.rgb, getAlpha(fragColor.rgb));
+}
