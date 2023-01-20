8def8587dec1919ba276bdd8b01f6d3771bea73b
MysteryPancake <MysteryPancake@users.noreply.github.com>
2023-01-20 21:12:42 +1100

    Add files via upload

--- glsl/BinarySearch2D.glsl
+++ glsl/BinarySearch2D.glsl
@@ -5,24 +5,12 @@ vec2 distort(vec2 uv) {
 	return uv * (1.0 - length(uv * 0.1));
 }
 
-// Functions to converge towards, use mouse to move target
+// Function to converge towards, use mouse to move target
 bvec2 check(vec2 guess) {
 	vec2 target = iMouse.z > 0.0 ?
 		distort((iMouse.xy - 0.5 * iResolution.xy) / iResolution.y)
 		: vec2(sin(iTime * 2.0) * 0.5, cos(iTime * 2.0) * 0.5);
 	return greaterThan(guess, target);
-}
-
-// Each step, move another half towards the target
-vec2 search(vec2 maximum, int steps) {
-	vec2 current = vec2(0.0);
-	vec2 move = maximum * 0.25;
-	for (int i = 0; i < steps; i++, move *= 0.5) {
-		bvec2 result = check(current);
-		current.x += result.x ? -move.x : move.x;
-		current.y += result.y ? -move.y : move.y;
-	}
-	return current;
 }
 
 // From https://iquilezles.org/articles/distfunctions2d
@@ -46,16 +34,25 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
 	vec2 grid = fract(uv * 16.0);
 	fragColor.rgb += vec3(step(0.9, max(grid.x, grid.y))) * 0.03;
 	
+	// Binary search
 	int steps = 6;
-	vec2 current = search(vec2(1.0), 0);
-	// Use current and next coordinate to draw lines
-	for (int i = 0; i < steps; i++) {
-		vec2 next = search(vec2(1.0), i + 1);
-		float dist = line(uv, current, next);
+	vec2 current = vec2(0.0);
+	vec2 move = vec2(0.25);
+	
+	for (int i = 0; i < steps; i++, move *= 0.5) {
+		// Store previous for line
+		vec2 last = current;
+		
+		// Move another half towards the target
+		bvec2 result = check(current);
+		current.x += result.x ? -move.x : move.x;
+		current.y += result.y ? -move.y : move.y;
+		
+		// Draw line
+		float dist = line(uv, last, current);
 		float percent = float(i) / float(steps);
 		vec3 hue = hue2rgb(percent);
 		fragColor.rgb += hue * (0.005 + percent * 0.005) / dist;
-		current = next;
 	}
 	
 	// Draw vignette
